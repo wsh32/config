d21765a01aee731300ae7c17eb100320c6151b54
Wesley Soo-Hoo <wesoohoo@gmail.com>
2019-03-05 22:44:03 -0800

    grab vimrc from github repo instead of gist

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ fi
 $SUDO apt-get update
 $SUDO apt-get install vim git
 
-# Get .vimrc from gist 
-wget -O ~/.vimrc https://gist.githubusercontent.com/wsh32/13ff83f0ca38eb463a3de233591848bd/raw
+# Get .vimrc from github repo  
+wget -O ~/.vimrc https://raw.githubusercontent.com/wsh32/vim/master/.vimrc
 
 # Get Vundle and install plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
